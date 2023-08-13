38eaf3bc678b9e04d367cc8a8f562e2c50031132
ideacatlab <razvan@ideacat.ro>
2023-08-13 22:33:08 +0300

    update to laravel 10

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-        require("postcss-import"),
-        require("postcss-advanced-variables"),
-        require("tailwindcss/nesting"),
-        require("tailwindcss")("./tailwind.config.js"),
-        require("autoprefixer"),
-    ],
-};
